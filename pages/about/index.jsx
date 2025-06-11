5577737e7001b3943d5f02ceb91b1b4ac26a16dd
SnehapriyaSajeevan <snehapriyasb9@gmail.com>
2025-06-11 16:11:10 +0530

    add

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,67 +1,55 @@
-import Loginform from '@/public/components/form/Loginform'
-import React from 'react'
-// import Loginform from '
-import { userState } from 'react'
+import React, { useState } from 'react'
 
 function index() {
-  import Head from "next/head";
-  import Image from "next/image";
-  import { Geist, Geist_Mono } from "next/font/google";
-  import styles from "@/styles/Home.module.css";
-  import React ,{useState} from "react";
-  
-  const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-  });
-  
-  const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-  });
-  
-  export default function Home() {
-    const [state,setState]=useState(0);
+   const [state,setState]=useState(0);
     const [name,setName]=useState("");
     const [email,setEmail]=useState(name+"gmail.com");
-    console.log("state",state);
-    console.log("Name",name);
-    console.log("email",email);
     const [password, setPassword]=useState("");
-    console.log("password",password);
-    
     function handleName (event) {
-      console.log(event.target.value);
-      setName(event.target.value);
-  
-    }
-    function pass (event) {
-      console.log(event.target.value);
-      setPassword(event.target.value);
-    }
-    function Email (event) {
-      console.log(event.target.value);
-      setEmail(event.target.value);
-    
-    }
-    function handleSubmit() {
-    if (password.length<=8) {
-      alert("password must be atleast 8 charactrs");
-      return;
-  
-    }
-  console.log("login successful");
-    }
-  return (
-    <Loginform/>
+    console.log(event.target.value);
+    setName(event.target.value);
 
+  }
+  function pass (event) {
+    console.log(event.target.value);
+    setPassword(event.target.value);
+  }
+  function Email (event) {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  
+  }
+  function handleSubmit() {
+  if (password.length<=8) {
+    alert("password must be atleast 8 charactrs");
+    return;
+
+  }}
+  return (
+    <>
+  <h1>
+    email={email}
+    {password}
+  
+  </h1>
+
+  {/* <input type='text' onChange={}/> */}
+  <p>
+    enter your username
+   </p>
+   <input type='text' onChange={handleName}/>
+   <p>
+    enter your password
+   </p><input type='password' onChange={pass}/>
+    <p>
+    enter your email
+   </p><input type='text' onChange={Email}/>
+   <button onClick={handleSubmit}>Submit</button>
+  
+    
+  </>
 
   )
 }
-
-    
-    
-  
-
 
 export default index
